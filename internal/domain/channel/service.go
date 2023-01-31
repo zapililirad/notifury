5af074f5b0e9702c13e7045dad041ebabd4f8699
Vladislav Popov <zapililirad@gmail.com>
2023-02-01 00:29:51 +0300

    20230131

--- internal/domain/channel/service.go
+++ internal/domain/channel/service.go
@@ -1,5 +1,16 @@
 package channel
 
+import (
+	"context"
+
+	"github.com/zapililirad/notifury/internal/domain/user"
+)
+
 type NotificationChannelService struct {
 	storage Storage
+}
+
+func (s *NotificationChannelService) GetNotificationChannelsByUser(ctx context.Context, u *user.User) []NotificationChannel {
+	// TODO: Implement GetNotificationChannelsByUser
+	return nil
 }
--- internal/domain/router/service.go
+++ internal/domain/router/service.go
@@ -3,36 +3,61 @@ package router
 import (
 	"context"
 
-	"github.com/zapililirad/notifury/internal/app"
 	"github.com/zapililirad/notifury/internal/domain/access"
+	"github.com/zapililirad/notifury/internal/domain/channel"
 	"github.com/zapililirad/notifury/internal/domain/group"
 	"github.com/zapililirad/notifury/internal/domain/message"
 	"github.com/zapililirad/notifury/internal/domain/policy"
+	"github.com/zapililirad/notifury/internal/domain/user"
 )
 
 type RouterService struct {
-	policyService *policy.DistributionPolicyService
-	groupService  *group.GroupService
+	policyService  *policy.DistributionPolicyService
+	groupService   *group.GroupService
+	channelService *channel.NotificationChannelService
+}
+
+func NewRouterService(
+	p *policy.DistributionPolicyService,
+	g *group.GroupService,
+	c *channel.NotificationChannelService,
+) *RouterService {
+	return &RouterService{
+		policyService:  p,
+		groupService:   g,
+		channelService: c,
+	}
 }
 
 func (s *RouterService) PushMessage(ctx context.Context, message *message.Message) error {
-	// TODO: Make me pretty
+	// TODO: Make me pretty - need to refactor
 
 	policy, err := s.policyService.GetDistributionPoliciesByMessageClass(ctx, message.Class)
 	if err != nil {
 		return err
 	}
 
+	users := []*user.User{}
+
 	for _, sp := range policy.Subscribers {
 
 		if sp.GetSecurityPrincipalType() == access.User {
-
+			users = append(users, sp.(*user.User))
 		}
 
 		if sp.GetSecurityPrincipalType() == access.Group {
-			continue
+			users = append(users, s.groupService.GetAllUsersRecursive(ctx, sp.(*group.Group))...)
 		}
 	}
 
-	return app.ErrNotImplemented
+	for _, u := range users {
+		channels := s.channelService.GetNotificationChannelsByUser(ctx, u)
+
+		for _, c := range channels {
+			c.PushMessage(*message)
+		}
+
+	}
+
+	return nil
 }
